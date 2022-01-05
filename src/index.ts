36795c630ef7f752984615b76c1074ada60e3217
DmPrkp <f89522795153@gmail.com>
2022-01-05 14:03:55 +0300

    initial type orm

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,77 @@
-import "reflect-metadata";
+// import "reflect-metadata";
 import {createConnection} from "typeorm";
 import {User} from "./entity/User";
-import Fastify, { FastifyInstance, RouteShorthandOptions } from 'fastify'
-import { Server, IncomingMessage, ServerResponse } from 'http'
+// import Fastify, { FastifyInstance, RouteShorthandOptions } from 'fastify'
+// import { Server, IncomingMessage, ServerResponse } from 'http'
 
-createConnection().then(async connection => {
+const PGHOST_DB = process.env.PGHOST_DB
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
+// const server: FastifyInstance = Fastify({})
 
-    console.log("Here you can setup and run express/koa/any other framework.");
+// const opts: RouteShorthandOptions = {
+//     schema: {
+//         response: {
+//             200: {
+//                 type: 'object',
+//                 properties: {
+//                     pong: {
+//                         type: 'string'
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
 
-}).catch(error => console.log(error));
+// server.get('/ping', opts, async (request, reply) => {
+//     return { pong: 'it worked!' }
+// })
 
-const server: FastifyInstance = Fastify({})
+// const start = async () => {
+//     try {
+//         await server.listen(3000, "0.0.0.0")
 
-const opts: RouteShorthandOptions = {
-    schema: {
-        response: {
-            200: {
-                type: 'object',
-                properties: {
-                    pong: {
-                        type: 'string'
-                    }
-                }
-            }
-        }
-    }
-}
+//         // const address = server.server.address()
+//         // const port = typeof address === 'string' ? address : address?.port
 
-server.get('/ping', opts, async (request, reply) => {
-    return { pong: 'it worked!' }
-})
+//     } catch (err) {
+//         server.log.error(err)
+//         process.exit(1)
+//     }
+// }
+// start()
 
-const start = async () => {
-    try {
-        await server.listen(3000)
+const connection = createConnection({
+    type: "postgres",
+    host: PGHOST_DB,
+    port: 5432,
+    username: "postgres",
+    password: "bot",
+    database: "bot",
+    synchronize: true,
+    logging: false,
+    entities: [
+        __dirname + "/entity/*.ts"
+    ],
+});
 
-        // const address = server.server.address()
-        // const port = typeof address === 'string' ? address : address?.port
+connection
+    .then(async connection => {
 
-    } catch (err) {
-        server.log.error(err)
-        process.exit(1)
-    }
-}
-start()
+        console.log("Inserting a new user into the database...");
+        const user = new User();
+        console.log('user', user)
+        user.firstName = "Timber";
+        user.lastName = "Saw";
+        user.age = 25;
+        await connection.manager.save(user);
+        console.log("Saved a new user with id: " + user.id);
+
+        console.log("Loading users from the database...");
+        const users = await connection.manager.find(User);
+        console.log("Loaded users: ", users);
+
+        console.log("Here you can setup and run express/koa/any other framework.");
+
+    }).catch(error => console.log('createConnection', error));
