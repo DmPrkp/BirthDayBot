45c6ab5a84214a9bc3d5471b4dab577a02c2ebf5
Dmitriy Prokopovich <prokopovich@indins.ru>
2022-01-19 20:56:12 +0300

    test request

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,23 @@ import { User } from "./entity/User";
 import Fastify, { RouteShorthandOptions } from 'fastify'
 import { FastifyORMInterface } from './interface/typeOrmPlugin'
 import { resolve } from 'path';
-// import { bootstrap } from 'fastify-decorators';
+import * as https from "https";
+// import * as TelegramBot from 'node-telegram-bot-api'
 import dbConnector from './plugin/db-connector'
+// import { bootstrap } from 'fastify-decorators';
+
+let token = '1823718690:AAHVu1ZMQWvXFC182G_e7K55yv7WjVpEgpQ'
+// const bot = new TelegramBot(token, {polling: true});
+// bot.onText(/\/echo (.+)/, (msg, match) => {
+//     const chatId = msg.chat.id;
+//     const resp = match[1];
+//     bot.sendMessage(chatId, resp);
+// });
+// bot.on('message', (msg) => {
+//     const chatId = msg.chat.id;
+//     bot.sendMessage(chatId, 'test');
+// });
+
 const PGHOST_DB: string = process.env.PGHOST_DB || 'localhost'
 const dbSettings = {
     type: "postgres",
@@ -26,7 +41,7 @@ const server: FastifyORMInterface = Fastify({
     logger: true
 })
 server.register(dbConnector, dbSettings)
-server.register(require('fastify-autoroutes'), {dir: '../routes'})
+// server.register(require('fastify-autoroutes'), {dir: '../routes'})
 
 
 // server.register(bootstrap, {
@@ -49,31 +64,7 @@ const opts: RouteShorthandOptions = {
     }
 }
 
-// server.get('/', opts, async (request, reply) => {
-    
-//     const birthday = new Birthday();
-//         birthday.day = '11.07'
-//         birthday.name = 'test'
-//         birthday.status = 'not notified' 
-//         await server.orm.manager.save(birthday);   
 
-//     const user = new User();   
-//         user.name = "Timber";
-//         user.telegramId = 25;
-//         user.birthdays = [birthday]
-//         await server.orm.manager.save(user);
-    
-//     if (server) {
-//         const userRepository = await server.orm.getRepository(User);
-//         const users = await userRepository.find({ relations: ["birthdays"] }); 
-//         console.log("Loaded users: ", JSON.stringify(users));
-//         return JSON.stringify(users)   
-//     }
-// })
-
-// server.get('/ping', opts, async (request, reply) => {        
-//    return { pong: 'it worked!' }
-// })
 
 const start = async () => {
     try {
@@ -84,5 +75,59 @@ const start = async () => {
         server.log.error(err)
         process.exit(1)
     }
+
+    let requestOptions = {
+        host: 'api.telegram.org',
+        path: '/bot'+token+'/getUpdates',
+        port: 443,
+        method: 'POST'
+    }
+
+    try {
+        let req = await https.request(requestOptions, res => {
+            res.setEncoding('utf8');
+            let data
+            res.on('data', d => data = d )
+            return data
+        })
+        req.on('error', error => {
+            console.error(error)
+        })
+
+        req.end()
+    } catch (err) {
+        console.error(err)
+        throw err
+    }
+
+
+
+
 }
 start()
+
+// server.get('/', opts, async (request, reply) => {
+
+//     const birthday = new Birthday();
+//         birthday.day = '11.07'
+//         birthday.name = 'test'
+//         birthday.status = 'not notified'
+//         await server.orm.manager.save(birthday);
+
+//     const user = new User();
+//         user.name = "Timber";
+//         user.telegramId = 25;
+//         user.birthdays = [birthday]
+//         await server.orm.manager.save(user);
+
+//     if (server) {
+//         const userRepository = await server.orm.getRepository(User);
+//         const users = await userRepository.find({ relations: ["birthdays"] });
+//         console.log("Loaded users: ", JSON.stringify(users));
+//         return JSON.stringify(users)
+//     }
+// })
+
+// server.get('/ping', opts, async (request, reply) => {
+//    return { pong: 'it worked!' }
+// })
--- src/plugin/fastify-telegram.ts
+++ src/plugin/fastify-telegram.ts
@@ -1,9 +1,27 @@
 import fastifyPlugin from "fastify-plugin";
 import { FastifyInstance, FastifyPluginCallback } from 'fastify'
+import * as http from "http";
+
+let token = '1823718690:AAHVu1ZMQWvXFC182G_e7K55yv7WjVpEgpQ'
 
 async function telegramPoolingConnection(fastify: FastifyInstance, options) {
+
+    let reqestOptions = {
+        hostname: 'https://api.telegram.org/bot'+token,
+        port: 443,
+        method: 'POST'
+    }
+
     try {
-        let connection
+        let req = http.request(reqestOptions, res => {
+            res.on('data', d => console.log(d))
+        })
+        req.on('error', error => {
+            console.error(error)
+        })
+
+        req.end()
+
     } catch (err) {
         console.error(err)
         throw err
