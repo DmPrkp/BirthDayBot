1e4ae7d9571ca2fc55d102bb34fadd1bcc34c19b
Dmitriy Prokopovich <prokopovich@indins.ru>
2022-02-25 21:48:30 +0300

    fix plugin bug

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,27 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 import Fastify, { FastifyInstance, RouteShorthandOptions } from 'fastify'
-import  fastifyTelegram from './plugin/fastify-telegram'
+// import  fastifyTelegram from './plugin/fastify-telegram'
+import {FastifyBotControllerInterface} from './interface/botControllerPlugin'
 import dbConnector from './plugin/db-connector'
+import {TelegramMessage, TelegramResponseBody, TelegramResultArray} from "./interface/telegramPlugin";
+import telegramFetch from "./services/telegram-fetch";
+import botController from "./plugin/bot-controller";
 
-const fastify: FastifyInstance = Fastify({logger: true})
+const fastify: FastifyBotControllerInterface = Fastify({logger: true})
 
 if (process.env.DB_USER && process.env.PASSWORD && process.env.ROOT_HOST) {
     fastify.register(dbConnector)
 }
 
-let token : string | undefined = process.env.TELEGRAM_TOKEN;
+// let token : string | undefined = process.env.TELEGRAM_TOKEN;
 
-(token) ? fastify.register(fastifyTelegram) : console.error('add env var TELEGRAM_TOKEN!');
+// (token) ? fastify.register(fastifyTelegram) : console.error('add env var TELEGRAM_TOKEN!');
+
+fastify.register(botController)
 
 const start = async () => {
+
     try {
         await fastify.listen(3000)
 
@@ -22,6 +29,33 @@ const start = async () => {
         fastify.log.error(err)
         process.exit(1)
     }
+
+    let reqOptions = {
+        method: 'getUpdates',
+        params: {timeout: 10, offset: 0}
+    }
+
+    let requestIterator = async (options) => {
+        let resp : TelegramResponseBody;
+        let offset: number;
+        try {
+            resp = await telegramFetch(options)
+            if (resp.ok && resp.result && resp.result.length) {
+                await fastify.bot(resp.result)
+            }
+        } catch (err) {
+            console.error(err);
+        }
+        let result : TelegramResultArray = resp.result;
+        console.log("result", result)
+        let lastMessage : TelegramMessage = (result.length) ? result.pop() : { update_id: options.params.offset }
+        offset = lastMessage.update_id
+        console.log('offset', offset)
+        reqOptions.params.offset = offset
+        console.log(reqOptions.params)
+        return await requestIterator(reqOptions)
+    }
+    await requestIterator(reqOptions)
 }
 
 start()
--- src/plugin/fastify-telegram.ts
+++ src/plugin/fastify-telegram.ts
@@ -6,32 +6,32 @@ import telegramFetch from "../services/telegram-fetch"
 
 async function telegramPoolingConnection(fastify: FastifyBotControllerInterface) {
 
-    await fastify.register(botController)
+    // await fastify.register(botController)
+    //
+    // let reqOptions = {
+    //     method: 'getUpdates',
+    //     params: {timeout: 10, offset: 883704889}
+    // }
 
-    let reqOptions = {
-        method: 'getUpdates',
-        params: {timeout: 10, offset: 883704889}
-    }
-
-    let requestIterator = async (options) => {
-        let resp : TelegramResponseBody;
-        let offset: number;
-        try {
-            resp = await telegramFetch(options)
-            if (resp.ok && resp.result && resp.result.length) {
-                await fastify.bot(resp.result)
-            }
-        } catch (err) {
-            console.error(err);
-        }
-        let result : TelegramResultArray = resp.result;
-        let lastMessage : TelegramMessage = (result) ? result.pop() : { update_id: options.params.offset }
-        offset = lastMessage.update_id
-        reqOptions.params.offset = offset + 1
-        console.log(reqOptions.params)
-        return await requestIterator(reqOptions)
-    }
-    await requestIterator(reqOptions)
+    // let requestIterator = async (options) => {
+    //     let resp : TelegramResponseBody;
+    //     let offset: number;
+    //     try {
+    //         resp = await telegramFetch(options)
+    //         if (resp.ok && resp.result && resp.result.length) {
+    //             await fastify.bot(resp.result)
+    //         }
+    //     } catch (err) {
+    //         console.error(err);
+    //     }
+    //     let result : TelegramResultArray = resp.result;
+    //     let lastMessage : TelegramMessage = (result) ? result.pop() : { update_id: options.params.offset }
+    //     offset = lastMessage.update_id
+    //     reqOptions.params.offset = offset + 1
+    //     console.log(reqOptions.params)
+    //     return await requestIterator(reqOptions)
+    // }
+    // await requestIterator(reqOptions)
 
 }
 
@@ -56,7 +56,7 @@ async function telegramPoolingConnection(fastify: FastifyBotControllerInterface)
 // return JSON.stringify(users)
 // }
 
-export default fastifyPlugin(telegramPoolingConnection, {
-    fastify: '>= 1.0.0',
-    name: 'fastify-telegram'
-})
+// export default fastifyPlugin(telegramPoolingConnection, {
+//     fastify: '>= 1.0.0',
+//     name: 'fastify-telegram'
+// })
